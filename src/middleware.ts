53c435b2975c0aeb7707f7a16274419fd00e137a
daddygi <vlimbaga49@gmail.com>
2025-04-05 08:47:45 +0800

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,18 @@
-//
-export const config = {
-    matcher: '/about/:path*',
+// src/middleware.ts
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const isLoggedIn = request.cookies.get("sb-access-token");
+
+  if (
+    request.nextUrl.pathname.startsWith("/dashboard") ||
+    request.nextUrl.pathname.startsWith("/admin")
+  ) {
+    if (!isLoggedIn) {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
   }
+
+  return NextResponse.next();
+}
