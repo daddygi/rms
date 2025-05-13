80040986d6de327f9af3915ad1686834e9b75c8a
daddygi <vlimbaga49@gmail.com>
2025-05-14 02:39:59 +0800

    public api

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,9 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
+  if (req.nextUrl.pathname.startsWith("/api")) {
+    return NextResponse.next();
+  }
   const res = NextResponse.next();
   const supabase = createMiddlewareClient({ req, res });
 
